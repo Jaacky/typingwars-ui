c8b8dbac70d6e70ec42b2da471415306ee4958e3
Jacky <jaackyvan@gmail.com>
2018-09-10 14:33:16 -0400

    Adding HP and username to base

--- src/components/GameMap.js
+++ src/components/GameMap.js
@@ -14,16 +14,18 @@ const GameMap = (props) => {
     const adjustRatioHeight = height / 100;
     console.log("gamemap: ", adjustRatioWidth, adjustRatioHeight);
     const viewBox = [0, 0, width, height];
+
+    var hp = 50;
     return (
         <div className={styles.GameMap}>
             Game Map
             <svg
-                preserveAspectRatio="xMaxYMax none"
+                preserveAspectRatio="xMidYMid"
                 viewBox={viewBox}
             >
-                <Base adjustRatioWidth={adjustRatioWidth} adjustRatioHeight={adjustRatioHeight} x={5} y={50}/>
-                <Base adjustRatioWidth={adjustRatioWidth} adjustRatioHeight={adjustRatioHeight} x={95} y={50}/>
-            
+                <Base adjustRatioWidth={adjustRatioWidth} adjustRatioHeight={adjustRatioHeight} x={5} y={50} hp={hp} user="abc"/>
+                <Base adjustRatioWidth={adjustRatioWidth} adjustRatioHeight={adjustRatioHeight} x={95} y={50} hp={hp} user="def"/>
+                
                 {/* <path d="M 20 20 C 20 110, 110 110, 110 20" stroke="black" fill="transparent"/> */}
             </svg>
         </div>
--- src/components/svg/Base.js
+++ src/components/svg/Base.js
@@ -6,11 +6,20 @@ const Base = (props) => {
     const x = props.x * props.adjustRatioWidth;
     const y = props.y * props.adjustRatioHeight;
     const path = `M${x - baseSize/2} ${y - baseSize/2} h ${baseSize} v ${baseSize} h ${-baseSize} Z`;
-    
+    console.log(props.hp);
     return (
-        <path
-            d={path} fill="transparent" stroke="black" stroke-width="8" stroke-linejoin="round"
-        />
+        <g>
+            <path
+                d={path} fill="transparent" stroke="black" strokeWidth="8" strokeLinejoin="round"
+            />  
+            <text
+                x={x} y={y}
+                textAnchor="middle" 
+            >
+                <tspan>{props.hp}</tspan>
+                <tspan x={x} y={y + 15}>{props.user}</tspan>
+            </text>
+        </g>
         // <circle cx={props.x * props.adjustRatioWidth} cy={props.y * props.adjustRatioHeight} r={5} />
     )
 }
